392431ba60ee30d07eaf622d62501adf216884b8
App Academy Student <student@appacademy.io>
2015-07-30 11:15:14 -0700

    Fade out tabs

--- tabs/js/tabs.js
+++ tabs/js/tabs.js
@@ -9,11 +9,15 @@
   $.Tabs.prototype.clickTab = function (e) {
     e.preventDefault();
     this.$activeTab.removeClass('active');
-    this.$el.find('.active').removeClass('active');
-    var $newTab = $(e.currentTarget);
-    $newTab.addClass('active');
-    this.$activeTab = $($newTab.attr("for"));
-    $($newTab.attr("for")).addClass('active');
+    this.$activeTab.addClass('transitioning');
+    this.$activeTab.one('transitionend', function () {
+      this.$activeTab.removeClass('transitioning');
+      this.$el.find('.active').removeClass('active');
+      var $newTab = $(e.currentTarget);
+      $newTab.addClass('active');
+      this.$activeTab = $($newTab.attr("for"));
+      $($newTab.attr("for")).addClass('active');
+    }.bind(this));
   };
 
   $.fn.tabs = function () {
