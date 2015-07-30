e464ad51b7169f9034c857d0c2e7cf8be8f5081c
App Academy Student <student@appacademy.io>
2015-07-30 11:08:10 -0700

    Add tab hiding and showing

--- tabs/js/tabs.js
+++ tabs/js/tabs.js
@@ -1,7 +1,24 @@
-$.Tabs = function (el) { ... };
+(function($) {
+  $.Tabs = function (el) {
+    this.$el = $(el);
+    this.$contentTabs = $(this.$el.data("content-tabs"));
+    this.$activeTab = this.$contentTabs.find(".active");
+    this.$el.on('click', 'a', this.clickTab.bind(this));
+  };
 
-$.fn.tabs = function () {
-  return this.each(function () {
-    new $.Tabs(this);
-  });
-};
+  $.Tabs.prototype.clickTab = function (e) {
+    e.preventDefault();
+    this.$activeTab.removeClass('active');
+    this.$el.find('.active').removeClass('active');
+    var $newTab = $(e.currentTarget);
+    $newTab.addClass('active');
+    this.$activeTab = $($newTab.attr("for"));
+    $($newTab.attr("for")).addClass('active');
+  };
+
+  $.fn.tabs = function () {
+    return this.each(function () {
+      new $.Tabs(this);
+    });
+  };
+})(jQuery);
